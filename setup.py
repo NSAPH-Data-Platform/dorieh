b9f0902725319359995760b2de5b01f1313717b7
mmcentre <michael.bouzinier@kvitneset.com>
2024-05-16 19:36:56 -0400

    MAB  Added new admission variables (see https://github.com/NSAPH-Data-Platform/nsaph-cms/pull/16)

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as readme:
 
 setup(
     name='dorieh',
-    version="0.0.6",
+    version="0.0.7",
     url='https://github.com/NSAPH-Data-Platform/dorieh',
     license='Apache 2.0',
     author='Michael A Bouzinier',
